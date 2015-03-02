97ab88f8c3e98811f4218c80f851e09c25c8b6ad
Nathan Smith <nathan@hebemedia.com>
2015-03-02 16:46:51 +0000

    Added Dashboard > list > tabs

---
+++ app/dashboard/list/tabs/controller.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+    actions: {
+        showTab: function(tab){
+            console.log('tab = '+tab);
+            // add dashboard-library-category-view
+            return true;
+        }
+    }
+});
