7480c82e21de0a3477306766e530b7bf547a7ce0
Andy Fitch <andy@parall.ax>
2015-03-03 15:06:17 +0000

    Undo svgstore

--- Brocfile.js
+++ Brocfile.js
@@ -2,14 +2,7 @@
 
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
-var app = new EmberApp({
-  //svgstore: {
-  //  files: {
-  //    sourceDirs: 'public/assets/img/svg',
-  //    outputFile: '/../../public/assets/img/svg/sprite.svg'
-  //  }
-  //}
-});
+var app = new EmberApp();
 
 // Use `app.import` to add additional libraries to the generated
 // output files.
@@ -23,7 +16,5 @@ var app = new EmberApp({
 // modules that you would like to import into your application
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
-
-//app.import('bower_components/svg4everybody/svg4everybody.min.js');
 
 module.exports = app.toTree();
