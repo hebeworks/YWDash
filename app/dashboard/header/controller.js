e60185ebb6802e516b401f16ea44150c3fd5bfbe
Andy Fitch <andy@parall.ax>
2015-03-17 11:14:12 +0000

    Top bar working with icons

--- app/dashboard/header/controller.js
+++ app/dashboard/header/controller.js
@@ -6,10 +6,19 @@ export default Ember.Controller.extend({
     }.property(),
 
     navItems: function(){
-        return [
+
+        var items = [
             { title: 'Toolbox', align: 'center', action: 'toggleSubNav' },
             { title: 'Gallery', align: 'right', action: 'toggleGallery' }
         ];
+
+        for (var i = 0; i < items.length; i++) {
+            items[i].dashed = '-' + items[i].title.dasherize();
+            items[i].iconclass = 'icon' + items[i].dashed;
+            items[i].svgclass = 'svg' + items[i].dashed;
+        }
+
+        return items;
     }.property(),
 
     //actions: {
