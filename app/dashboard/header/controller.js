fda81f8d6da77521de1f146a22302a914e23b8ee
Andy Fitch <andy@parall.ax>
2015-03-17 12:52:47 +0000

    Top drawer with icons, separator, data bound etc

--- app/dashboard/header/controller.js
+++ app/dashboard/header/controller.js
@@ -21,6 +21,21 @@ export default Ember.Controller.extend({
         return items;
     }.property(),
 
+    topDrawerNavItems: function(){
+
+        var items = [
+            { title: 'Change Canvas', jshook: 'js-toolbox-change-canvas', iconclass: 'icon-change-canvas', svgclass: 'svg-change-canvas' },
+            { title: 'Add a story', jshook: 'js-add-story', iconclass: 'icon-add-a-story', svgclass: 'svg-add-a-story' },
+            { title: 'Duplicate', jshook: 'js-toolbox-duplicate', iconclass: 'icon-duplicate', svgclass: 'svg-duplicate' },
+            { title: 'Edit meta', jshook: 'js-toolbox-edit-meta', iconclass: 'icon-edit-meta', svgclass: 'svg-edit-meta', isSeparator: true },
+            { title: 'Share', jshook: 'js-toolbox-share', iconclass: 'icon-share', svgclass: 'svg-share' },
+            { title: 'Presentation', jshook: 'js-toolbox-presentation', iconclass: 'icon-presentation', svgclass: 'svg-presentation' },
+            { title: 'Help', jshook: 'js-toolbox-help', iconclass: 'icon-help', svgclass: 'svg-help' }
+        ];
+
+        return items;
+    }.property(),
+
     //actions: {
     //    gotoRoute:function(route){
     //        this.get('target').transitionTo(route);
