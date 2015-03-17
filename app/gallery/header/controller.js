89596d69bd11ca978a914d5c369235e98c6e00d0
Andy Fitch <andy@parall.ax>
2015-03-17 15:11:07 +0000

    Gallery header, icons, controller stuff

--- app/gallery/header/controller.js
+++ app/gallery/header/controller.js
@@ -3,13 +3,6 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
     siteTitle: function(){
         return 'Leeds Gallery Title';
-    }.property(),
-
-    navItems: function(){
-        return [
-            { title: 'Toolbox', align: 'center', action: 'toggleSubNav' },
-            { title: 'Gallery', align: 'right', action: 'toggleGallery' }
-        ];
     }.property()
 
 });
