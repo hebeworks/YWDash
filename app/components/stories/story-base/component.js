bbf43b46efbf073e403c38992377024517c3768b
Barnaby Cotter <Barnaby@parall.ax>
2015-03-20 17:12:14 +0000

    Pass through config options to parent template

--- app/components/stories/story-base/component.js
+++ app/components/stories/story-base/component.js
@@ -1,4 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+    tagName: 'div',
+    classNames: ['story'],
+    classNameBindings: ['color', 'width', 'height'],
+    attributeBindings: ['data-ss-colspan']
 });
