a2cca66931596ea4c0402dee00c0a4946ae6c929
Nathan Smith <nathan@hebemedia.com>
2015-04-21 10:16:15 +0100

    Clock style tweaks

--- app/components/stories/complete/clock-face/component.js
+++ app/components/stories/complete/clock-face/component.js
@@ -4,7 +4,7 @@ export default Ember.Component.extend({
     tagName: 'div',
     loaded: false,
     didInsertElement: function() {
-        this.set('title', 'Time');
+        this.set('title', '');
         this.set('subTitle', '');
         this.setupClock();
     },
