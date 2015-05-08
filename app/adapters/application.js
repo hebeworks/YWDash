221d8512854d89384fc32951a91b298495de5616
Nathan Smith <nathan@hebemedia.com>
2015-05-08 11:25:00 +0100

    Horizontally Centered the canvases

--- app/adapters/application.js
+++
@@ -1,7 +0,0 @@
-import DS from 'ember-data';
-
-export default DS.RESTAdapter.extend({
-    namespace: 'api'
-});
-
-//export default DS.FixtureAdapter.extend();
--- app/components/canvas-switcher/component.js
+++ app/components/canvas-switcher/component.js
@@ -1,5 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-	
+	canvases: null,
+	getCanvases: function () {
+		var obj = this;
+		this.store.find('canvas')
+			.then(function (canvases) {
+			obj.set('canvases', canvases);
+		});
+	}.on('init')
 });
