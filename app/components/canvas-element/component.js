dd287553bd62d638c7335aee411703a491aa1512
Nathan Smith <nathan@hebemedia.com>
2015-05-08 16:24:58 +0100

    Temp hidden API for deploy

--- app/components/canvas-element/component.js
+++ app/components/canvas-element/component.js
@@ -2,11 +2,16 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 	tagName: 'li',
-	getStories: function(){
-		var obj = this;
-		this.store.find('story')
-			.then(function (stories) {
-                obj.set('stories', stories);
-            });
-	}.on('init')
+//	getStories: function(){
+//		var obj = this;
+//        var model = this.get('model');
+//		debugger;
+//		if(model.get('stories') != null){
+//			
+//		} else {
+//			this.store.find('story').then(function (stories) {
+//                obj.set('stories', stories);
+//            });
+//		}
+//	}.on('init')
 });
