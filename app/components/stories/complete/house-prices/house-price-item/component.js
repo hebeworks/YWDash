dcacdeaddc3b0531f1ffaa1bd59b53fe4f037cb9
Nathan Smith <nathan@hebemedia.com>
2015-05-20 16:55:11 +0100

    Added router Google analytics tracking code Working on house price chart

--- app/components/stories/complete/house-prices/house-price-item/component.js
+++ app/components/stories/complete/house-prices/house-price-item/component.js
@@ -8,14 +8,23 @@ export default Ember.Component.extend({
     graphParams: {
         data: {
             columns: [
-                ['data1', 30, 200, 100, 400, 150, 250],
+                ['leeds', 190, 193, 192, 195, 196, 199, 198, 200],
+                ['national', 210, 210, 210, 210, 210, 210, 210, 210],
+                ['x', 'Jan', 'Feb', 'March', 'April', 'May', 'June', 'July', 'August']
+//                ['data1', 30, 200, 100, 400, 150, 250],
 //                ['data2', 130, 100, 140, 200, 150, 50]
             ],
             type: 'line',
             colors: {
                 data1: '#7ED321',
-//                data2: '#2980B9'
+                data2: '#2980B9'
+            },
+            axis: {
+                x: {
+                    type: 'timeseries'
+                }
             }
         }
     }
+
 });
