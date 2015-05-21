a4d177a62421f137c69781416394ff8ca2762fd1
Nathan Smith <nathan@hebemedia.com>
2015-05-21 14:30:41 +0100

    Cleaned up some jshint errors fixed clock

--- app/components/stories/story-types/default-story/component.js
+++ app/components/stories/story-types/default-story/component.js
@@ -4,7 +4,7 @@ import Config from './../../../../config/environment';
 export default Ember.Component.extend({
 	config: Config.APP,
 
-	getData: function (url, cache) {
+	getData: function (url) {
 		var obj = this;
 		return new Ember.RSVP.Promise(function (resolve, reject) {
 			try
@@ -13,7 +13,7 @@ export default Ember.Component.extend({
 				if(obj.isCORS(url)) {
 					obj.crossDomainAjax(url, function (data) {
 						resolve(data);
-					}, cache);				
+					});				
 				} else {
 					return Ember.$.getJSON(url)
 						.then(function(data){
@@ -33,7 +33,7 @@ export default Ember.Component.extend({
 	},
 	
 
-	crossDomainAjax: function (url, successCallback, cache) {
+	crossDomainAjax: function (url, successCallback) {
 		// IE8 & 9 only Cross domain JSON GET request
 		if ('XDomainRequest' in window && window.XDomainRequest !== null) {
 			var xdr = new XDomainRequest(); // Use Microsoft XDR
@@ -63,7 +63,7 @@ export default Ember.Component.extend({
 		else {
 			$.ajax({
 				url: url,
-				cache: (cache === true ? true : false),
+				cache: false,
 				dataType: 'json',
 				type: 'GET',
 				async: false, // must be set to false
