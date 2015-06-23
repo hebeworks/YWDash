d191b9870f6c125e45fe31aa312f73d8f988e3b4
Simon Zimmerman <simon@hebeworks.com>
2015-06-23 14:26:18 +0100

    Started York River Levels

--- app/components/stories/complete/york-river/component.js
+++ app/components/stories/complete/york-river/component.js
@@ -1,30 +1,61 @@
-import Ember from 'ember';
+import DefaultStory from './../../story-types/default-story/component';
 
-export default Ember.Component.extend({
+export default DefaultStory.extend({
     tagName: 'div',
     loaded: false,
-    didInsertElement: function() {
-        this.set('title', 'York Rivers');
-        this.set('subTitle', 'york-river SUB TITLE');
-        this.fetchData();
-    },
-    fetchData: function() {
-        Ember.$.getJSON('http://thecitytalking.com/content/?format=json&callback=?').then((data) => {
-            data.items.forEach((item) => {
-                // format API data here
-                item.date = moment(item.publishOn).format('MMMM DD, YYYY');
-                item.url = 'http://thecitytalking.com' + item.fullUrl;
-                item.excerpt = this.stripHTML(item.excerpt).substring(0, 100) + '...';
+    selectedStation: null,
+    didInsertElement: function () {
+        this.set('title', 'Live River Level');
+        this.set('subTitle', 'Water flowing under Crown Point Bridge');
+        var obj = this;
+        this.getData('http://environment.data.gov.uk/flood-monitoring/id/stations/L1707', true)
+            .then(function (station) {
+                var item = station.items;
+                var latestReading = item.measures.latestReading;
+                var maxOnRecord = item.stageScale.maxOnRecord;
+                var minOnRecord = item.stageScale.minOnRecord;
+                var highestRecent = item.stageScale.highestRecent;
+                var typicalRangeHigh = item.stageScale.typicalRangeHigh;
+                var typicalRangeLow = item.stageScale.typicalRangeLow;
+                obj.setProperties({
+                    latestReading: latestReading,
+                    maxOnRecord: maxOnRecord,
+                    minOnRecord: minOnRecord,
+                    highestRecent: highestRecent,
+                    typicalRangeHigh: typicalRangeHigh,
+                    typicalRangeLow: typicalRangeLow,
+                });
+                // check for changes to the api data every fifteen minutes
+                setTimeout(obj.fetchData, 900000);
+                setTimeout(function () {
+                    obj.set('loaded', true);
+                });
             });
-            this.set('items', data.items);
-            setTimeout(() => {
-                this.set('loaded', true);
-            });
+    }.observes('selectedStation'),
+
+    setHeights: function () {
+        var obj = this;
+        var heightLowest = ((this.get("minOnRecord.value") / this.get("maxOnRecord.value")) * 100);
+        var heightTypicalHigh = (((this.get("typicalRangeHigh") / this.get("maxOnRecord.value")) * 100) - heightLowest);
+        var heightHighestRecent = (((this.get("highestRecent.value") / this.get("maxOnRecord.value")) * 100) - (heightLowest + heightTypicalHigh));
+        var heightMax = (100 - (heightLowest + heightTypicalHigh + heightHighestRecent));
+        obj.setProperties({
+            heightLowest: heightLowest,
+            heightTypicalHigh: heightTypicalHigh,
+            heightHighestRecent: heightHighestRecent,
+            heightMax: heightMax,
         });
-    },
-    stripHTML: function(html) {
-        var div = document.createElement("div");
-        div.innerHTML = html;
-        return div.textContent || div.innerText || "";
-    }
+        var heightLowestString = heightLowest.toString() + '%';
+        this.$(".river-levels-columnChart-container-inner--lowestLevel").height(heightLowestString);
+        var heightTypicalHighString = heightTypicalHigh.toString() + '%';
+        this.$(".river-levels-columnChart-container-inner--typicalHighLevel").height(heightTypicalHighString);
+        var heightHighestRecentString = heightHighestRecent.toString() + '%';
+        this.$(".river-levels-columnChart-container-inner--highestLevelRecent").height(heightHighestRecentString);
+        var heightMaxString = heightMax.toString() + '%';
+        this.$(".river-levels-columnChart-container-inner--maxLevel").height(heightMaxString);
+    }.observes("minOnRecord.value", "typicalRangeHigh", "maxOnRecord.value", "highestRecent")
 });
+
+
+
+
