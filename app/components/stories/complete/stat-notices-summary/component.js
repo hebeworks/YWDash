b524fbdba582606338fdac23113cb60eb8b36a58
Nathan Smith <nathan@hebemedia.com>
2015-06-24 13:29:05 +0100

    added initial stat notices summary story

---
+++ app/components/stories/complete/stat-notices-summary/component.js
@@ -0,0 +1,28 @@
+/* global Ember, hebeutils, _ */
+import DefaultStory from './../../story-types/default-story/component';
+
+export default DefaultStory.extend({
+    tagName: 'div',
+    loaded: false,
+    didInsertElement: function () {
+        this.set('title', 'Statutory notices');
+        this.set('subTitle', 'Newly added near you');
+        var obj = this;
+                obj.set('newlyAddedCount', 4);
+
+//        this.store.find('StatNotices')
+//            .then(function (items) {
+//                // Todo: how to define new?
+//                    // start date - so search for items newer then startDate
+//                obj.set('newlyAddedCount', 4);
+//                setTimeout(() => {
+//                    obj.set('loaded', true);
+//                });
+//            });
+    },
+    
+    click: function() {
+        this.set('action','gotoRoute');
+        this.sendAction('action', 'statnotices');
+    }
+});
