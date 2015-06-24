4de1a5b634fb6e81e98479301b61d6a2a82f8548
Nathan Smith <nathan@hebemedia.com>
2015-06-24 20:46:06 +0100

    added all stories for http://statnotices.leedsdatamill.org deploy

--- app/components/stories/complete/stat-notices-summary/component.js
+++ app/components/stories/complete/stat-notices-summary/component.js
@@ -8,17 +8,19 @@ export default DefaultStory.extend({
         this.set('title', 'Statutory notices');
         this.set('subTitle', 'Added this month');
         var obj = this;
-                obj.set('newlyAddedCount', 4);
-
-//        this.store.find('StatNotices')
-//            .then(function (items) {
-//                // Todo: how to define new?
-//                    // start date - so search for items newer then startDate
 //                obj.set('newlyAddedCount', 4);
-//                setTimeout(() => {
-//                    obj.set('loaded', true);
-//                });
-//            });
+
+        this.getData('http://statnotices.azurewebsites.net/api/statnotices?count=true')
+            .then(function (count) {
+                // Todo: how to define new?
+                    // start date - so search for items newer then startDate
+                obj.set('newlyAddedCount', count);
+                setTimeout(() => {
+                    obj.set('loaded', true);
+                });
+            },function(err){
+                console.log('Error getting stat notice count: ' + err);
+            });
     },
     
     click: function() {
