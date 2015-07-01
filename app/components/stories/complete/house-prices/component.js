4b48b0c3346fe1b845924f973f92d3ca6791d8c0
Simon Zimmerman <simon@hebeworks.com>
2015-07-01 11:58:51 +0100

    Story updates.

--- app/components/stories/complete/house-prices/component.js
+++ app/components/stories/complete/house-prices/component.js
@@ -10,7 +10,7 @@ export default DefaultStory.extend({
 	loadedMonths: 0,
 	didInsertElement: function () {
 		this.set('title', 'House prices in Leeds');
-		this.set('subTitle', 'Enquiries dealt with by Leeds City Council');
+		this.set('subTitle', 'Average house price in Leeds by month');
 		this.getPast12Months();
 	},
 
